1dc54e822c17e22a7f1c0e4a1028798a18960e17
Sara OC <saramoo@hotmail.com>
2015-04-29 15:30:35 +0100

    add firefox multicapabilities

--- test/e2e/conf.js
+++ test/e2e/conf.js
@@ -1,7 +1,9 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['githubProfileFeature.js'],
-  capabilities: {
+  multiCapabilities: [{
+    browserName: 'firefox'
+  }, {
     browserName: 'chrome'
-  }
+  }]
 }
